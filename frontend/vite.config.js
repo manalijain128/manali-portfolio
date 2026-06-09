1307ef56d890878a0ee6db1b69089637cb00de8d
manalijain128 <manalijain128@gmail.com>
2026-06-09 18:35:25 +0530

    added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
-  base: '/manali-portfolio/',   // 👈 MUST be your repo name
+  plugins: [react(), tailwindcss()],
+  base: '/manali-portfolio/',  // 👈 MUST match repo name exactly
 })
